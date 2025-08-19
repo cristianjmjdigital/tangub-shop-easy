cf194ee24c769d7345a06a02a9b0110794934cf2
cristianjmjdigital <cristian@jmjdigital.uk>
2025-08-19 22:57:56 +0800

    Create capacitor.config.ts

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.tangub.shopeasy',
+  appName: 'Tangub Shop Easy',
+  webDir: 'dist'
+};
+
+export default config;
